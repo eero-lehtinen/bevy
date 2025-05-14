8ee698628c3e7a36594e2042b314c99497ee6b0f
Eero Lehtinen <eero@eerolehtinen.fi>
2025-05-15 01:14:15 +0300

    Increase decal count

--- crates/bevy_pbr/src/decal/clustered.rs
+++ crates/bevy_pbr/src/decal/clustered.rs
@@ -62,7 +62,7 @@ pub(crate) const CLUSTERED_DECAL_SHADER_HANDLE: Handle<Shader> =
 /// This number is currently relatively low in order to work around the lack of
 /// first-class binding arrays in `wgpu`. When that feature is implemented, this
 /// limit can be increased.
-pub(crate) const MAX_VIEW_DECALS: usize = 8;
+pub(crate) const MAX_VIEW_DECALS: usize = 16;
 
 /// A plugin that adds support for clustered decals.
 ///
